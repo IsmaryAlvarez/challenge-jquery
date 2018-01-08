f6bbfabcbc9d6c499d3d5d831400c5e3a0e589f3
kuveee <kamilavillablanca23@gmail.com>
2018-01-08 12:49:50 -0300

    ocultando div etapa 5

--- js/script-index.js
+++ js/script-index.js
@@ -4,6 +4,8 @@ $(document).ready( function(){
 	renderHighlightedRecipes(recipesArray);
 
 	printNews();
+
+	renderActivities();
 });
 
 
@@ -32,7 +34,7 @@ function renderHighlightedRecipes(recipesArray) {
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
-	$(".list-recipes").append('<a class="item-recipe" href="#"><span class="attribution"><span class="title-recipe">' + recipe.title + ' </span> <span class="metadata-recipe"> <span class="author-recipe"> ' + recipe.source.name + '</span> <span class="bookmarks-recipe"> <span class="icon-bookmark"></span> </span> </span></span> <img src="img/recipes/320x350/fall-mexican-rice.jpg" /> </a>');
+	$(".list-recipes").append('<a class="item-recipe" href="#"><span class="attribution"><span class="title-recipe">' + recipe.title + ' </span> <span class="metadata-recipe"> <span class="author-recipe"> ' + recipe.source.name + '</span> <span class="bookmarks-recipe"> <span class="icon-bookmark"></span> </span> </span></span> <img src="img/recipes/320x350/'+ recipe.name +'.jpg" /> </a>');
 
 }
 
@@ -41,7 +43,10 @@ function renderRecipe(recipe) {
 * Función que se encarga de pintar todas las actividades
 */
 function renderActivities(activitiesArray) {
-	console.log('Activities: ', activitiesArray);
+	if (activities.length > 0){
+		$(".wrapper-message").hide();
+	}
+	console.log('Activities: ', activities);
 }
 
 /*
